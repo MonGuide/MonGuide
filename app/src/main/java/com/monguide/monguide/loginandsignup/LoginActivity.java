069a8cfb9cea5a5d2a10517ff15ed9aafe9fb01d
laidbacklevi <saurabhnegi9654313343@gmail.com>
2019-11-03 22:26:28 +0530

    commit

--- app/src/main/java/com/monguide/monguide/loginandsignup/LoginActivity.java
+++ app/src/main/java/com/monguide/monguide/loginandsignup/LoginActivity.java
@@ -1,17 +1,31 @@
 package com.monguide.monguide.loginandsignup;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.monguide.monguide.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // if user is already logged in
+        if(mAuth.getCurrentUser() != null) {
+            //new Intent(this, HomeActivity.class);
+        }
 
     }
 }
