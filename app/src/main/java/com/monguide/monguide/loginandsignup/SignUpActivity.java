ed9d35f777ac346c4b179910de47d22e88780cd4
nerdcoder21 <piyush.2018mnnit@gmail.com>
2019-11-06 20:23:19 +0530

    sign_up_layout_fully_done

--- app/src/main/java/com/monguide/monguide/loginandsignup/SignUpActivity.java
+++ app/src/main/java/com/monguide/monguide/loginandsignup/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.monguide.monguide.loginandsignup;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.*;
@@ -22,10 +25,17 @@ import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    private Button mSignupButton;
+    private static int RESULT_LOAD_IMAGE = 1;
+    private ImageView mimageView;
     private EditText mUsernameEditText;
     private EditText mEmailEditText;
     private EditText mPasswordEditText;
+    private EditText mCollegeNameEditText;
+    private EditText mCourseNameEditText;
+    private EditText mGraduationYearEditText;
+    private EditText mCompanyNameEditText;
+    private EditText mJobProfileEditText;
+    private Button mSignupButton;
     private TextView mLoginTextView;
 
     private FirebaseAuth mAuth;
@@ -39,52 +49,26 @@ public class SignUpActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mDatabaseReference = FirebaseDatabase.getInstance().getReference();
 
-        mSignupButton = (Button) findViewById(R.id.activity_signup_signupButton);
-        mUsernameEditText = (EditText) findViewById(R.id.activity_signup_nameEditText);
-        mEmailEditText = (EditText) findViewById(R.id.activity_signup_emailEditText);
-        mPasswordEditText = (EditText) findViewById(R.id.activity_signup_passwordEditText);
-        mLoginTextView = (TextView) findViewById(R.id.activity_signup_loginTextView);
+        mSignupButton = (Button) findViewById (R.id.activity_signup_signupButton);
+        mUsernameEditText = (EditText) findViewById (R.id.activity_signup_nameEditText);
+        mEmailEditText = (EditText) findViewById (R.id.activity_signup_emailEditText);
+        mPasswordEditText = (EditText) findViewById (R.id.activity_signup_passwordEditText);
+        mLoginTextView = (TextView) findViewById (R.id.activity_signup_loginTextView);
+        mimageView = (ImageView) findViewById (R.id.activity_signup_imageView);
+        mCollegeNameEditText = (EditText) findViewById (R.id.activity_signup_collegeNameEditText);
+        mCourseNameEditText = (EditText) findViewById (R.id.activity_signup_courseName_EditText);
+        mGraduationYearEditText = (EditText) findViewById (R.id.activity_signup_graduationYearEditText);
+        mCompanyNameEditText = (EditText) findViewById (R.id.activity_signup_companyNameEditText);
+        mJobProfileEditText = (EditText) findViewById (R.id.activity_signup_jobProfile_EditText);
+
 
         mSignupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = mEmailEditText.getText().toString();
-                String password = mPasswordEditText.getText().toString();
-                DatabaseReference temp = mDatabaseReference.child("users/XNdIoSHChJWhliVOTlEgQ8VfGol1");
-                temp.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
-                        Log.e("XXX", map.toString());
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {}
-
-                });
-
-                /*
-                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            String mUID = mAuth.getCurrentUser().getUid();
-                            DatabaseReference currUserDatabaseReference = mDatabaseReference.child("users/" + mUID);
-                            String name = mUsernameEditText.getText().toString();
-                            UserDetails.EducationDetails educationDetails = new UserDetails.EducationDetails("a", "b", 1);
-                            UserDetails.WorkDetails workDetails = new UserDetails.WorkDetails("aa", "bb");
-                            UserDetails user = new UserDetails(name, educationDetails, workDetails);
-                            currUserDatabaseReference.setValue(user);
-                            Toast.makeText(SignUpActivity.this, "Success!", Toast.LENGTH_LONG).show();
-                            startHomeActivity();
-                        } else {
-                            Toast.makeText(SignUpActivity.this, "Registration Unsuccessful", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-                */
+                if(checkProfileDetails() && checkEducationDetails() && checkWorkDetails())
+                    sendToDatabase();
             }
         });
-
 
         mLoginTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,8 +76,43 @@ public class SignUpActivity extends AppCompatActivity {
                 startLoginActivity();
             }
         });
+
+        mimageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
     }
 
+
+    private void sendToDatabase(){
+        String email = mEmailEditText.getText().toString();
+        String password = mPasswordEditText.getText().toString();
+        DatabaseReference temp = mDatabaseReference.child("users/XNdIoSHChJWhliVOTlEgQ8VfGol1");
+        temp.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                Log.e("XXX", map.toString());
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+            Uri imageAddress = data.getData();
+            mimageView.setImageURI(imageAddress);
+        }
+    }
 
     private void startLoginActivity() {
         startActivity(new Intent(this, LoginActivity.class));
@@ -103,5 +122,66 @@ public class SignUpActivity extends AppCompatActivity {
     private void startHomeActivity() {
         startActivity(new Intent(this, HomeActivity.class));
         finish();
+    }
+
+    private boolean checkWorkDetails() {
+
+        if(TextUtils.isEmpty(mCompanyNameEditText.getText()) && TextUtils.isEmpty(mJobProfileEditText.getText())){
+            return true;
+        }
+
+        if(!TextUtils.isEmpty(mCompanyNameEditText.getText()) && !TextUtils.isEmpty(mJobProfileEditText.getText())){
+            return true;
+        }
+
+        if(TextUtils.isEmpty(mCompanyNameEditText.getText()) && !TextUtils.isEmpty(mJobProfileEditText.getText())){
+            mCompanyNameEditText.setError("Company name required");
+            return false;
+        }
+
+        else{
+            mJobProfileEditText.setError("Job profile required");
+            return false;
+        }
+    }
+
+    private boolean checkEducationDetails() {
+
+        if(TextUtils.isEmpty(mCollegeNameEditText.getText())){
+            mCollegeNameEditText.setError("College name required");
+            return false;
+        }
+
+        if(TextUtils.isEmpty(mCourseNameEditText.getText())){
+            mCourseNameEditText.setError("Course name required");
+            return false;
+        }
+
+        if(TextUtils.isEmpty(mGraduationYearEditText.getText())){
+            mGraduationYearEditText.setError("Graduation year required.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean checkProfileDetails() {
+
+        if(TextUtils.isEmpty(mUsernameEditText.getText())){
+            mUsernameEditText.setError("Name required");
+            return false;
+        }
+
+        if(TextUtils.isEmpty(mPasswordEditText.getText())){
+            mPasswordEditText.setError("Password required");
+            return false;
+        }
+
+        if(TextUtils.isEmpty(mEmailEditText.getText())){
+            mEmailEditText.setError("Email required");
+            return false;
+        }
+
+        return true;
     }
 }
