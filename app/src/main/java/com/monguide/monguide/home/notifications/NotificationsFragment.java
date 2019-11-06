77f304803aa092b37ab7187a938a6fab1774763d
uttamrana333 <ranauttam333@gmail.com>
2019-11-07 00:04:11 +0530

    layout_notification_item

--- app/src/main/java/com/monguide/monguide/home/notifications/NotificationsFragment.java
+++ app/src/main/java/com/monguide/monguide/home/notifications/NotificationsFragment.java
@@ -15,6 +15,6 @@ public class NotificationsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_notifications, container, false);
+        return inflater.inflate(R.layout.fragment_notification, container, false);
     }
 }
