ff2caf1d06bb9b08bd35c80191eb241e8bcb75bf
nerdcoder21 <piyush.2018mnnit@gmail.com>
2019-11-06 22:29:48 +0530

    sign_up_activty_data_upload

--- app/src/main/java/com/monguide/monguide/utils/DatabaseHelper.java
+++ app/src/main/java/com/monguide/monguide/utils/DatabaseHelper.java
@@ -2,12 +2,16 @@ package com.monguide.monguide.utils;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+
 public class DatabaseHelper {
+
     public static DatabaseReference getReferenceToRoot() {
         return FirebaseDatabase.getInstance().getReference();
     }
@@ -24,8 +28,14 @@ public class DatabaseHelper {
         return getReferenceToRoot().child("questions");
     }
 
-    public static DatabaseReference getReferenceToParticularQuestions(String qid) {
+    public static DatabaseReference getReferenceToParticularQuestion(String qid) {
         return getReferenceToAllQuestions().child(qid);
     }
+
+    public static StorageReference getReferenceToStorage(){
+        return FirebaseStorage.getInstance().getReference();
+    }
+
+
 
 }
