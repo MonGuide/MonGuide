43337538fe33c90837ba75d75db0530e37ea9d43
laidbacklevi <saurabhnegi9654313343@gmail.com>
2019-11-07 01:12:01 +0530

    Implemented RecyclerView for home feed.

--- app/src/main/java/com/monguide/monguide/home/feed/FeedFragment.java
+++ app/src/main/java/com/monguide/monguide/home/feed/FeedFragment.java
@@ -8,21 +8,54 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 import com.monguide.monguide.R;
+import com.monguide.monguide.models.question.Question;
+import com.monguide.monguide.utils.QuestionSummaryAdapter;
+import com.monguide.monguide.utils.QuestionSummaryHolder;
+import com.monguide.monguide.utils.DatabaseHelper;
 
 public class FeedFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private QuestionSummaryAdapter mQuestionSummaryAdapter;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        View inflatedView = inflater.inflate(R.layout.fragment_feed, container, false);
+
+        mRecyclerView = (RecyclerView) inflatedView.findViewById(R.id.fragment_feed_recyclerview);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mQuestionSummaryAdapter = new QuestionSummaryAdapter();
+        mRecyclerView.setAdapter(mQuestionSummaryAdapter);
+
+        for (int i = 0; i < 30; i++) {
+            Question question = new Question("This is title " + i, "This is body " + i);
+            mQuestionSummaryAdapter.addQuestionToQuestionsList(question);
+        }
+
+        populateFeed();
+        return inflatedView;
     }
 
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    private void populateFeed() {
+        DatabaseHelper.getReferenceToAllQuestions().addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/monguide/monguide/utils/DatabaseHelper.java
+++ app/src/main/java/com/monguide/monguide/utils/DatabaseHelper.java
@@ -5,11 +5,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-
 public class DatabaseHelper {
 
     public static DatabaseReference getReferenceToRoot() {
@@ -30,10 +25,6 @@ public class DatabaseHelper {
 
     public static DatabaseReference getReferenceToParticularQuestion(String qid) {
         return getReferenceToAllQuestions().child(qid);
-    }
-
-    public static StorageReference getReferenceToStorage(){
-        return FirebaseStorage.getInstance().getReference();
     }
 
 
